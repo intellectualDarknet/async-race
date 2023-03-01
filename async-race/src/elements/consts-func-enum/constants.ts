17cf1823f1aedf716feb866954185b13cf691ec1
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2023-03-01 15:12:13 +0500

    feat: change base url

--- async-race/src/elements/consts-func-enum/constants.ts
+++ async-race/src/elements/consts-func-enum/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'intellectualdarknet-JSFE2022Q1/#';
+export const BASE_URL = '';
 export const SERVER_URL = 'https://async-race-back.onrender.com/';
 export const carXmark = [
   'LFA',
